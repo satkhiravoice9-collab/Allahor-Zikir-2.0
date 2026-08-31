5ba09123bab1f80abfd23bf31d24783a8d1058fb
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-08-31 12:27:07 +0600

    Configure plugin and dependency management in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
-Ok
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Muslim Time"
+include(":app")
