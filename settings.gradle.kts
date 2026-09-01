7edfc766d84ddf510433246415f943bc8d2cdd25
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-09-01 19:46:54 +0600

    Add JitPack repository to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // নিচের লাইনটি নতুন যোগ করা হয়েছে PDF লাইব্রেরি ডাউনলোড করার জন্য
+        maven { url = uri("https://jitpack.io") } 
     }
 }
 
