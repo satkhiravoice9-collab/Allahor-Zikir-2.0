ec09e727bfae7ae1b3bd391f38b7f1d7fb2ab8d3
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-09-03 22:06:27 +0600

    Add Google Services plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    // ফায়ারবেস প্লাগইন এখানে যোগ করা হলো
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
