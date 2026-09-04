a281a7187860cea5c605988cdf62f4c78ce7094d
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-09-04 14:40:42 +0600

    Update Android and Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    // ফায়ারবেস প্লাগইন এখানে যোগ করা হলো
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
