9c0cbc3a274f8500d38b1cdbc1a7058ce7d6bc15
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-09-04 21:55:55 +0600

    Add Firebase Database dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     implementation("org.json:json:20210307")
